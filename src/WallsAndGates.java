e3d749654e630262291b3b59353f4528d8844057
Nauris <nauris.gruduls@gmail.com>
2023-04-23 22:30:23 +0300

    rooms and walls and gates

---
+++ src/WallsAndGates.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class WallsAndGates {
+    private static final int EMPTY = Integer.MAX_VALUE;
+    private static final int GATE = 0;
+    private static final List<int[]> DIRECTIONS = Arrays.asList(
+            new int[] { 1,  0},
+            new int[] {-1,  0},
+            new int[] { 0,  1},
+            new int[] { 0, -1}
+    );
+
+    public void wallsAndGates(int[][] rooms) {
+        int m = rooms.length;
+        if (m == 0) return;
+        int n = rooms[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (rooms[row][col] == GATE) {
+                    q.add(new int[] { row, col });
+                }
+            }
+        }
+        while(!q.isEmpty()) {
+            int[] cell = q.poll();
+            int row = cell[0];
+            int col = cell[1];
+            for (int[] direction : DIRECTIONS) {
+                int r = row + direction[0];
+                int c = col + direction[1];
+                if (r < 0 || c < 0 || r >= m || c >= n || rooms[r][c] != EMPTY) {
+                    continue;
+                }
+                rooms[r][c] = rooms[row][col] + 1;
+                q.add(new int[] { r, c });
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] rooms = {{2147483647,-1,0,2147483647}, {2147483647,2147483647,2147483647,-1},{2147483647,-1,2147483647,-1}, {0,-1,2147483647,2147483647}};
+        WallsAndGates a1 = new WallsAndGates();
+        a1.wallsAndGates(rooms);
+        for (int row = 0; row < rooms.length; row++) {
+            System.out.println(Arrays.toString(rooms[row]));
+        }
+    }
+}
